bb13bd388f4a0e37d2d99cfd073955b4e17dad25
graysonmorris <morris31@colostate.edu>
2024-04-29 18:48:56 -0600

    Changes to be committed:     modified:   Code/Assets/Goal.cs

--- Code/Assets/Goal.cs
+++ Code/Assets/Goal.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 
-public class Goal : MonoBehaviour
-{
-
+public class Goal : MonoBehaviour {
     public GameObject scoringController;
+    public float scoringThreshold = 1.0f; 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Ball"))
-        {
-            scoringController.SendMessage("GoalScored", gameObject.tag);
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Ball")) {
+            // Check if the ball is beyond the scoring threshold
+            if (IsBallBeyondThreshold(other.transform.position)) {
+                scoringController.SendMessage("GoalScored", gameObject.tag);
+            }
         }
+    }
+
+    private bool IsBallBeyondThreshold(Vector3 ballPosition) {
+        float distanceFromGoalLine = Vector3.Dot(ballPosition - transform.position, transform.forward);
+        return distanceFromGoalLine > scoringThreshold;
     }
 }
